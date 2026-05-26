d89c589eaef79c54a250e16de78cba560554fb1c
sriram-devops <mamidalasriram4@gmail.com>
2026-05-26 11:20:38 -0700

    project2 app and database

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+import psycopg2
+import os
+
+app = Flask(__name__)
+
+def get_db():
+    return psycopg2.connect(
+        host=os.environ.get("DB_HOST", "db"),
+        database=os.environ.get("DB_NAME", "devops"),
+        user=os.environ.get("DB_USER", "ram"),
+        password=os.environ.get("DB_PASS", "pavi123")
+    )
+
+@app.route("/")
+def home():
+    return "Project 2 — App + Database! 🚀"
+
+@app.route("/health")
+def health():
+    return "OK"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
